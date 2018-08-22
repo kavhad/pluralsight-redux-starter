f6ae99dcc9e9a64385a7705137943939d7972e86
Kaveh <kaveh.hadjari@b3it.se>
2018-08-22 16:55:46 +0200

    add courses page, menu links and routing

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,6 +5,8 @@ const Header = () =>
   <nav>
     <IndexLink to="/" activeClassName="active">Home</IndexLink>
     {" - "}
+    <Link to="/courses" activeClassName="active">Courses</Link>
+    {" - "}
     <Link to="/about" activeClassName="active">About</Link>
   </nav>;
 
