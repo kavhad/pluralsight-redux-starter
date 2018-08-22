91e2f9f4cc620fd460cd2c80d844b95c4f75b238
Kaveh <kaveh.hadjari@b3it.se>
2018-08-22 19:09:22 +0200

    Add async reading of course data with thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,19 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
-}
+export const createCourse = (course) => ({
+  type: types.CREATE_COURSE, course
+});
+
+export const loadCoursesSuccess = (courses) => ({
+  type: types.LOAD_COURSES_SUCCESS,
+  courses
+});
+
+export const loadCourses = () =>
+    (dispatch) =>
+      courseApi.getAllCourses().then(courses => {
+        dispatch(loadCoursesSuccess(courses));
+      }).catch(error => {
+        throw(error);
+      });
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,40 +2,44 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
  class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    this.state = {
-      course: { title: '' }
-    };
+    // this.state = {
+    //   course: { title: '' }
+    // };
 
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
+    // this.onTitleChange = this.onTitleChange.bind(this);
+    // this.onClickSave = this.onClickSave.bind(this);
 
   }
 
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
+  // onTitleChange(event) {
+  //   const course = this.state.course;
+  //   course.title = event.target.value;
+  //   this.setState({course: course});
+  // }
 
-  onClickSave(event) {
-    this.props.actions.createCourse(this.state.course);
-  }
+  // onClickSave(event) {
+  //   this.props.actions.createCourse(this.state.course);
+  // }
 
-  courseRow(course, index) {
-    return <div key={index}>{course.title}</div>;
-  }
+  // courseRow(course, index) {
+  //   return <div key={index}>{course.title}</div>;
+  // }
 
   render() {
+
+    const {courses} = this.props;
+
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
+        <CourseList courses={courses} />
+        {/* <h2>Add Course</h2>
         <input
           type="text"
           onChange={this.onTitleChange}
@@ -44,7 +48,7 @@ import * as courseActions from '../../actions/courseActions';
         <input
           type="submit"
           onClick={this.onClickSave}
-          value="Save" />
+          value="Save" /> */}
 
       </div>
     );
