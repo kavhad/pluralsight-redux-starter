29d931589ab035a50616a1bc5bbfe495b28e640e
Kaveh <kaveh.hadjari@b3it.se>
2018-08-24 11:12:23 +0200

    fix route

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,15 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
-import ManageCoursesPage from './components/course/ManageCoursePage';
+import ManageCoursePageConnected from './components/course/ManageCoursePage';
 
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage} />
         <Route path="courses" component={CoursesPage} />
-        <Route path="course" component={ManageCoursesPage} />
-        <Route path="course/:id" component={ManageCoursesPage} />
+        <Route path="course" component={ManageCoursePageConnected} />
+        <Route path="course/:id" component={ManageCoursePageConnected} />
         <Route path="about" component={AboutPage} />
     </Route>
 );
